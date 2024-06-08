857b0cdf6edf6c1b31b16cfd1c32785044220db5
Max Korop <makskorop99@gmail.com>
2024-06-08 20:40:01 +0300

    Added packages, added JwtStrategy and JwtAuthGuard

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe({
+		transform: true,
+		whitelist: true
+	}));
 	app.enableCors({ origin: "*" });
 	app.setGlobalPrefix('api');
 	const config = new DocumentBuilder()
 		.setTitle('API example')
 		.setDescription('Management API of users and their posts')
 		.setVersion('0.0.1')
-		.addTag('management')
+		.addTag('Management API')
+		.addBearerAuth(
+			{
+				type: 'http',
+				scheme: 'Bearer',
+				bearerFormat: 'JWT',
+				in: 'header',
+				name: 'Authorization',
+				description: 'Please enter token (JWT)',
+			},
+			'JWT-auth',
+		)
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api/docs', app, document);
---
+++ src/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { User } from "../user/user.schema";
+import { Model } from "mongoose";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectModel(User.name) private userModel: Model<User>,
+        private configService: ConfigService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: configService.get<string>("JWT_SECRET_KEY")
+        });
+    }
+
+    async validate(payload: any) {
+        const user = await this.userModel.findById(payload._id);
+        console.log(payload, user);
+        if (!user) throw new UnauthorizedException();
+        return user;
+    }
+}
